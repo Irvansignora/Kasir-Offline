192eff9d31b94aa78679affd7436a1a239d39b8f
Muhamad Irpan Yasin <143773773+Irvansignora@users.noreply.github.com>
2026-04-23 19:58:37 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // KasirKu PWA Service Worker
-// Mendukung: localhost, GitHub Pages, Netlify, dan hosting lainnya
-const CACHE_VERSION = 'v2';
+// Mendukung: localhost,
+const CACHE_VERSION = 'v3';
 const CACHE_NAME = 'kasirku-' + CACHE_VERSION;
 
 // File lokal yang di-cache (relative, akan di-resolve otomatis)
